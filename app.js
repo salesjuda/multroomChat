ba73d7c748fb5bbd331da6aa981178515754078f
judá sales <juda_sales@hotmail.com>
2019-11-21 00:20:58 -0300

    Seção !3 Aula 69

--- app.js
+++ app.js
@@ -19,12 +19,26 @@ io.on('connection', function(socket){
     });
 
     socket.on('msgParaServidor', function(data){
+
+        /* Dialogo */
         socket.emit('msgParaCliente', 
-        {apelido: data.apelido, mensagem: data.mensagem}
+            {apelido: data.apelido, mensagem: data.mensagem}
         );
             
         socket.broadcast.emit('msgParaCliente', 
             {apelido: data.apelido, mensagem: data.mensagem}
-            );    
+        );
+        
+        /* participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit('participantesParaCliente', 
+                {apelido: data.apelido}
+            );
+
+            socket.broadcast.emit('participantesParaCliente', 
+                {apelido: data.apelido}
+            );
+        }
+
     });
 });
